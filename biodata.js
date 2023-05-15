054e2b56edd38c1e0968615a2122eb341d9a8a30
Diaz Husni Fachriza <129046201+DiazHusni@users.noreply.github.com>
2023-05-16 06:26:16 +0700

    Add files via upload

--- biodata.js
+++ biodata.js
@@ -1,6 +1,25 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/api/biodata', function(req, res) {
+    const nama = req.body.name;
+    const tempatLahir = req.body["tempat-lahir"];
+    const tanggalLahir = req.body["tanggal-lahir"];
+    const alamat = req.body.alamat;
+
+    res.send({
+        'nama': nama,
+        'tempat-lahir': tempatLahir,
+        'tanggal-lahir': tanggalLahir,
+        'alamat': alamat,
+    })
+});
+
 app.get('/biodata', function (req, res) {
     const nama = req.query.name;
     const tempatLahir = req.query["tempat-lahir"];
